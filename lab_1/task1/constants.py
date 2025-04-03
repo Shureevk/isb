a437ac97ec029a8de061bbbd564e0e28a62222d0
Kasum <k-shureev@mail.ru>
2025-04-03 21:45:56 +0300

    Все константы вынес в отдельный файл

---
+++ lab_1/task1/constants.py
@@ -0,0 +1,7 @@
+ALPHABET = "АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ "
+REVERSED_ALPHABET = ALPHABET[::-1]
+
+# Пути к файлам
+FILE_ORIGINAL_TEXT = "data/original_text.txt"
+FILE_ENCRYPTED_TEXT = "data/encrypted_text.txt"
+FILE_ENCRYPTION_KEY = "data/encryption_key.json"
