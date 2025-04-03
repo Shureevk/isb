58b551fef7181444042a3fca83551aca49bab500
Shureevk <164297560+Shureevk@users.noreply.github.com>
2025-04-03 21:50:35 +0300

    constants.py

--- lab_1/task1/constants.py
+++ lab_1/task1/constants.py
@@ -1,7 +1,6 @@
 ALPHABET = "АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ "
 REVERSED_ALPHABET = ALPHABET[::-1]
 
-# Пути к файлам
 FILE_ORIGINAL_TEXT = "data/original_text.txt"
 FILE_ENCRYPTED_TEXT = "data/encrypted_text.txt"
 FILE_ENCRYPTION_KEY = "data/encryption_key.json"
