51c25f23c3bf720385cebabbe8203733838c8dcc
Shureevk <164297560+Shureevk@users.noreply.github.com>
2025-05-30 00:06:35 +0500

    Delete lab_2 directory

--- lab_2/consts.py
+++
@@ -1,9 +0,0 @@
-PI = [0.2148, 0.3672, 0.2305, 0.1875]
-
-cpp_sequence_txt= 'cpp_sequence.txt'
-java_sequence_txt='java_sequence_txt'
-python_sequence_txt='python_sequence_txt'
-
-test_results_cpp='test_results_cpp.txt'
-test_results_java='test_results_java'
-test_results_python='test_results_python'
