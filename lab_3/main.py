a5d117dba74e90148753aa367ec34d8130c798b3
Kasum <k-shureev@mail.ru>
2025-05-29 23:52:24 +0500

    Добавлен режим шифрования данных

--- lab_3/main.py
+++ lab_3/main.py
@@ -34,3 +34,27 @@ def generate_keys(config: dict):
     except Exception as e:
         print(f"Key generation error: {str(e)}")
         sys.exit(1)
+
+def encrypt_data(config: dict):
+    """
+    Шифрует открытый текст с использованием гибридной криптосистемы.
+
+    :param config: Configuration dictionary with file paths
+    :raises SystemExit: If encryption fails
+    """
+    print("==== Encryption Mode ====")
+    try:
+        encrypted_key = AsymmetricCrypto.load_encrypted_key(config['encrypted_symmetric_key'])
+        private_key = AsymmetricCrypto.load_private_key(config['private_key'])
+        symmetric_key = EncryptionService.decrypt_key(encrypted_key, private_key)
+
+        plaintext = FileOperations.read_data(config['plaintext'])
+        iv, ciphertext = EncryptionService.encrypt_data(plaintext, symmetric_key)
+
+        FileOperations.write_data(config['encrypted_iv'], iv)  # New file for IV
+        FileOperations.write_data(config['encrypted_text'], ciphertext)
+
+        print("Data encrypted successfully")
+    except Exception as e:
+        print(f"Encryption failed: {str(e)}")
+        raise SystemExit(1)
